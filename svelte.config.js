ba34bc4c0bfc982ace905000865f3e613f1997c5
Whitey <c1783468@gmail.com>
2024-10-10 00:08:48 +0800

    fix base

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: '/TF2-Item-List/'
+		}
 	}
 };
 export default config;
